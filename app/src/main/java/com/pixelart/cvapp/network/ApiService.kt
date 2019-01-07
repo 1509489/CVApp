514690b5a21223cfbb115d5a170a3b821c86af43
Justice <popystone@gmail.com>
2019-01-07 10:00:12 +0000

    Added few more items to the json

--- app/src/main/java/com/pixelart/cvapp/network/ApiService.kt
+++ app/src/main/java/com/pixelart/cvapp/network/ApiService.kt
@@ -5,6 +5,6 @@ import io.reactivex.Single
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("86ca2208b7e186d3436cab8412124cb2/raw/482240b342fa8565e45d0f6b4c42dc2297d126e9/samplecv.json")
+    @GET("86ca2208b7e186d3436cab8412124cb2/raw/e2ddb435c6195b22e0d7b0cb65dd61aa840019f6/samplecv.json")
     fun getCvs(): Single<APIResponse>
 }
