257bc84c6984bae0440930c7de854e05a7792234
Justice <popystone@gmail.cm>
2019-04-15 19:08:53 +0100

    modified the HomePresenter to use kotlin lambda instead of anonymous classes

--- app/src/main/java/com/pixelart/cvapp/ui/homescreen/HomePresenterImpl.kt
+++ app/src/main/java/com/pixelart/cvapp/ui/homescreen/HomePresenterImpl.kt
@@ -1,32 +1,24 @@
 package com.pixelart.cvapp.ui.homescreen
 
-import com.pixelart.cvapp.model.APIResponse
 import com.pixelart.cvapp.network.ApiService
-import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class HomePresenterImpl @Inject constructor(private val view: HomeContract.View, private val apiService: ApiService): HomeContract.Presenter {
+    private val compositeDisposable = CompositeDisposable()
 
     override fun getCVs() {
-        apiService.getCvs()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : SingleObserver<APIResponse>{
-                override fun onSuccess(t: APIResponse) {
-                    view.showCVs(t.sampleCvs)
-                }
+        compositeDisposable.add(
+            apiService.getCvs()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .toObservable()
+                .subscribe({response -> view.showCVs(response.sampleCvs)},
+                    {error -> view.showError("Error: ${error.message}")})
+        )
 
-                override fun onSubscribe(d: Disposable) {
-
-                }
-
-                override fun onError(e: Throwable) {
-                    view.showError("Error: ${e.message}")
-                }
-            })
     }
 
     override fun onResume() {
@@ -34,6 +26,6 @@ class HomePresenterImpl @Inject constructor(private val view: HomeContract.View,
     }
 
     override fun onStop() {
-
+        compositeDisposable.clear()
     }
 }
